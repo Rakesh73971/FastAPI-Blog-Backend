b52b975d27bb7767a033798b2cce970087125dca
Rakesh N <nraki522@gmail.com>
2026-02-21 11:48:54 +0530

    updated post put method

--- app/routers/post.py
+++ app/routers/post.py
@@ -55,18 +55,26 @@ def delete_post(id:int,db:Session=Depends(get_db),current_user=Depends(get_curre
     return Response(status_code=status.HTTP_204_NO_CONTENT)
 
 
-@router.put("/{id}",response_model=Post)
-def update_post(id:int,post:PostCreate,db:Session=Depends(get_db),current_user=Depends(get_current_user)):
-    post_query = db.query(models.Post).filter(models.Post.id == id)
-    existing_post = post_query.first()
+@router.put("/{id}", response_model=Post)
+def update_post(
+    id: int,
+    post: PostCreate,
+    db: Session = Depends(get_db),
+    current_user=Depends(get_current_user),
+):
+    post_obj = db.query(models.Post).filter(models.Post.id == id).first()
 
-    if existing_post is None:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail=f'post with id:{id} does not exist')
-    
-    if existing_post.owner_id != current_user.id:
-        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN,detail='Not authorized to perform requested action')
+    if post_obj is None:
+        raise HTTPException(status_code=404, detail="Post not found")
 
-    post_query.update(post.dict(),synchronize_session=False)
+    if post_obj.owner_id != current_user.id:
+        raise HTTPException(status_code=403, detail="Not authorized")
+
+    post_obj.title = post.title
+    post_obj.content = post.content
+    post_obj.published = post.published
+
     db.commit()
-    updated = post_query.first()
-    return updated
+    db.refresh(post_obj)
+
+    return post_obj
